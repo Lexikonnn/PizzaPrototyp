a64ba4d0ddba45554199f66a0e3b0b009054c182
Jan Skrzeczek <h.skrzeczek@gmail.com>
2024-08-06 11:51:17 +0200

    get and post request added

--- server/routes/Pizzas.js
+++ server/routes/Pizzas.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { Pizzas } = require('../models');
 
-router.get('/', (req, res) => {
-    
+router.get('/', async (req, res) => {
+    const listOfPizzas = await Pizzas.findAll();
+    res.json(listOfPizzas);
 });
 
-
+router.post('/', async(req, res) => {
+    const pizza = req.body;
+    await Pizzas.create(pizza);
+    res.json(pizza);
+})
 
 
 module.exports = router;
